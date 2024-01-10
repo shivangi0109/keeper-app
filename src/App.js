9ff6b7d943e6c47e4b9028ad2ed4148dd9dc11f7
Shivangi Srivastava <shi06dec@gmail.com>
2024-01-10 15:37:51 -0700

    Create Note component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Note from "./components/Note";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <Header />
+      <Note />
       <Footer />
     </div>
   );
