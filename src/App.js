cdb1a1c634d035a6885253f640a72069fca54219
Shivangi Srivastava <shi06dec@gmail.com>
2024-01-17 22:45:59 -0700

    Create Input Area and styled it

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
 import notes from "./notes";
+import CreateArea from "./components/CreateArea";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <Header />
+      <CreateArea />
       {notes.map(note => (
           <Note key={note.key} title={note.title} content={note.content} />
         )
