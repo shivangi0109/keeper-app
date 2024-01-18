b9e6a4ffc665f00b111fa87d01f7c72db8352e0f
Shivangi Srivastava <shi06dec@gmail.com>
2024-01-17 23:43:52 -0700

    Add new note by adding a functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
-import notes from "./notes";
+// import notes from "./notes";
 import CreateArea from "./components/CreateArea";
 import "./App.css";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  console.log(notes);
+
+  const addItem = (note) => {
+    setNotes((prevNote) => {
+      return [...prevNote, note];
+    });
+  };
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      {notes.map(note => (
-          <Note key={note.key} title={note.title} content={note.content} />
+      <CreateArea onAdd={addItem} />
+      {notes.map((note, index) => (
+          <Note key={index} title={note.title} content={note.content} />
         )
       )}
       <Footer />
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,10 +1,34 @@
-function CreateArea() {
+import { useState } from "react";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+  console.log(note);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  };
+
+  const submitNote = (event) => {
+    event.preventDefault();
+    props.onAdd(note);
+  };
+
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+      <form onSubmit={submitNote}>
+        <input onChange={handleChange} name="title" placeholder="Title" value={note.title} />
+        <textarea onChange={handleChange} name="content" placeholder="Take a note..." rows="3" value={note.content} />
+        <button type="submit">Add</button>
       </form>
     </div>
   );
