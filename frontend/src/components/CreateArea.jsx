b0ba9f911e9804c9898068e5ea152683ce1e5601
Shivangi Srivastava <shi06dec@gmail.com>
2024-02-23 16:53:33 -0700

    Clean up code

--- frontend/src/components/CreateArea.jsx
+++ frontend/src/components/CreateArea.jsx
@@ -8,30 +8,9 @@ function CreateArea(props) {
   const formRef = useRef();
   const newNote = {};
   const [isExpanded, setIsExpanded] = useState(false);
-  // const [note, setNote] = useState({
-  //   title: "",
-  //   content: "",
-  // });
-  // console.log(note);
-
-  // const handleChange = (event) => {
-  //   const { name, value } = event.target;
-
-  //   setNote((prevNote) => {
-  //     return {
-  //       ...prevNote,
-  //       [name]: value,
-  //     };
-  //   });
-  // };
 
   const submitNote = (event) => {
     event.preventDefault();
-    // props.onAdd(note);
-    // setNote({
-    //   title: "",
-    //   content: "",
-    // })
 
     const formData = new FormData(event.target);
     newNote.title = formData.get('title');
